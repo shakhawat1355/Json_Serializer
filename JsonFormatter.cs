d911842aeb8cc71b90890967fcc15979048fe403
shakhawat1355 <hossain.shakhawat@brainstation-23.com>
2023-03-23 13:16:59 +0600

    added the files to the tree

---
+++ JsonFormatter.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using System.Text;
+
+namespace Assignemnt1
+{
+    public class JsonFormatter
+    {
+
+
+        public static string Convert(object obj)
+        {
+            StringBuilder jsonBuilder = new StringBuilder();
+            Serializer(obj, jsonBuilder);
+            return jsonBuilder.ToString();
+        }
+
+        public static void Serializer(object obj, StringBuilder jsonBuilder)
+        {
+            Type objType = obj.GetType();
+
+
+            jsonBuilder.Append("{");
+
+            bool InitialElemenet = true;
+            PropertyInfo[] properties = objType.GetProperties();
+
+
+            foreach (var property in properties)
+            {
+                var propertyValue = property.GetValue(obj);
+
+                if (propertyValue == null)
+                {
+                    continue;
+                }
+
+                if (!InitialElemenet) jsonBuilder.Append(",");
+
+
+                InitialElemenet = false;
+
+                jsonBuilder.Append("\"");
+                jsonBuilder.Append(property.Name);
+                jsonBuilder.Append("\":");
+
+                if (propertyValue is IList list) ListSerializer(list, jsonBuilder);
+
+                else if (propertyValue is string)
+                {
+                    jsonBuilder.Append("\"");
+                    jsonBuilder.Append(propertyValue.ToString());
+                    jsonBuilder.Append("\"");
+                }
+
+                else if (propertyValue.GetType().IsValueType)
+                {
+                    jsonBuilder.Append("\"");
+                    jsonBuilder.Append(propertyValue.ToString());
+                    jsonBuilder.Append("\"");
+                }
+
+
+                else
+                {
+                    Serializer(propertyValue, jsonBuilder);
+                }
+            }
+
+            jsonBuilder.Append("}");
+        }
+
+        public static void ListSerializer(IList list, StringBuilder jsonBuilder)
+        {
+            jsonBuilder.Append("[");
+
+            bool InitialElemenet = true;
+            foreach (var item in list)
+            {
+                if (!InitialElemenet)
+                {
+                    jsonBuilder.Append(",");
+                }
+                InitialElemenet = false;
+
+                if (item is string )
+                {
+                    jsonBuilder.Append("\"");
+                    jsonBuilder.Append(item.ToString());
+                    jsonBuilder.Append("\"");
+                }
+
+                else if (item.GetType().IsValueType)
+                {
+                    jsonBuilder.Append("\"");
+                    jsonBuilder.Append(item.ToString());
+                    jsonBuilder.Append("\"");
+                }
+
+                else
+                {
+                    Serializer(item, jsonBuilder);
+                }
+            }
+
+            jsonBuilder.Append("]");
+        }
+
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using Assignemnt1;
+using Assignment1;
+
+var course = new Course
+{
+    Title = "C# Fundamentals",
+    Teacher = new Instructor
+    {
+        Name = "Jalal Uddin",
+        Email = "jalal.exe@gmail.com",
+        PresentAddress = new Address
+        {
+            Street = "Mirpur 10",
+            City = "Dhaka",
+            Country = "Bangladesh"
+        },
+        PermanentAddress = new Address
+        {
+            Street = "Mirpur 10",
+            City = "Dhaka",
+            Country = "Bangladesh"
+        },
+        PhoneNumbers = new List<Phone>
+        {
+            new Phone
+            {
+                Number = "180XXXXXXX",
+                Extension = "",
+                CountryCode = "+880"
+            }
+        }
+    },
+    Topics = new List<Topic>
+    {
+        new Topic
+        {
+            Title = "Intro to C#",
+            Description = "Learn the basics of C# language",
+            Sessions = new List<Session>
+            {
+                new Session
+                {
+                    DurationInHour = 2,
+                    LearningObjective = "Understand the syntax and new features of C# language"
+                }
+            }
+        }
+    },
+    Fees = 8000,
+    Tests = new List<AdmissionTest>
+    {
+        new AdmissionTest
+        {
+            StartDateTime = new DateTime(2022, 01, 01, 10, 0, 0),
+            EndDateTime = new DateTime(2022, 01, 01, 12, 0, 0),
+            TestFees = 100
+        }
+    }
+};
+
+
+
+//var test = course.GetType().GetProperties();
+
+
+//Console.WriteLine(test[4]);
+
+string json = JsonFormatter.Convert(course);
+Console.WriteLine(json);
